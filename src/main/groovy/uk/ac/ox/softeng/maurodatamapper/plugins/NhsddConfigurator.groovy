8e302906064f75245f3ee89e30cacb25ed341b48
Oliver Freeman <oliver.freeman@bdi.ox.ac.uk>
2022-02-17 11:49:44 +0000

    Update to the full mdm-core 5.1.0-SNAPSHOT and remove the logback file
    Add a custom configurator which extends the Mdm one for controlling logging.
    We will probably want to remove this, but it makes it easier to control what logging happens

---
+++ src/main/groovy/uk/ac/ox/softeng/maurodatamapper/plugins/NhsddConfigurator.groovy
@@ -0,0 +1,22 @@
+package uk.ac.ox.softeng.maurodatamapper.plugins
+
+import uk.ac.ox.softeng.maurodatamapper.core.logback.MdmConfigurator
+
+import static ch.qos.logback.classic.Level.INFO
+
+/**
+ * @since 16/02/2022
+ */
+class NhsdConfigurator extends MdmConfigurator {
+
+    @Override
+    void configureLogLevels() {
+        super.configureLogLevels()
+        addInfo("Adding additional logger levels for NHSDD testing")
+        setLoggerLevel 'uk.ac.ox.softeng.maurodatamapper.terminology.CodeSetService', INFO
+        setLoggerLevel 'uk.ac.ox.softeng.maurodatamapper.terminology.TerminologyService', INFO
+        setLoggerLevel 'uk.ac.ox.softeng.maurodatamapper.datamodel.DataModelService', INFO
+        setLoggerLevel 'uk.ac.ox.softeng.maurodatamapper.core.facet.BreadcrumbTreeService', INFO
+        setLoggerLevel 'uk.ac.ox.softeng.maurodatamapper.core.model.ModelService', INFO
+    }
+}
