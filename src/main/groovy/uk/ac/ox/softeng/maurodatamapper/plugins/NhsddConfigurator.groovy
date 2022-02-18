17af18a83e7c55c908e97ab56c2cda6eceba0353
Oliver Freeman <oliver.freeman@bdi.ox.ac.uk>
2022-02-18 13:07:25 +0000

    Use reWriteBatchInserts for PG and reenable logging for the model services

--- src/main/groovy/uk/ac/ox/softeng/maurodatamapper/plugins/NhsddConfigurator.groovy
+++ src/main/groovy/uk/ac/ox/softeng/maurodatamapper/plugins/NhsddConfigurator.groovy
@@ -2,8 +2,6 @@ package uk.ac.ox.softeng.maurodatamapper.plugins
 
 import uk.ac.ox.softeng.maurodatamapper.core.logback.MdmConfigurator
 
-import static ch.qos.logback.classic.Level.INFO
-
 /**
  * @since 16/02/2022
  */
@@ -13,10 +11,10 @@ class NhsddConfigurator extends MdmConfigurator {
     void configureLogLevels() {
         super.configureLogLevels()
         addInfo("Adding additional logger levels for NHSDD testing")
-        setLoggerLevel 'uk.ac.ox.softeng.maurodatamapper.terminology.CodeSetService', INFO
-        setLoggerLevel 'uk.ac.ox.softeng.maurodatamapper.terminology.TerminologyService', INFO
-        setLoggerLevel 'uk.ac.ox.softeng.maurodatamapper.datamodel.DataModelService', INFO
-        setLoggerLevel 'uk.ac.ox.softeng.maurodatamapper.core.facet.BreadcrumbTreeService', INFO
-        setLoggerLevel 'uk.ac.ox.softeng.maurodatamapper.core.model.ModelService', INFO
+        //        setLoggerLevel 'uk.ac.ox.softeng.maurodatamapper.terminology.CodeSetService', INFO
+        //        setLoggerLevel 'uk.ac.ox.softeng.maurodatamapper.terminology.TerminologyService', INFO
+        //        setLoggerLevel 'uk.ac.ox.softeng.maurodatamapper.datamodel.DataModelService', INFO
+        //        setLoggerLevel 'uk.ac.ox.softeng.maurodatamapper.core.facet.BreadcrumbTreeService', INFO
+        //        setLoggerLevel 'uk.ac.ox.softeng.maurodatamapper.core.model.ModelService', INFO
     }
 }
