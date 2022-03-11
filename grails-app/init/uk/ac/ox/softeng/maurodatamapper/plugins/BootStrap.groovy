2ba694b868389e2d959d8c9636e0a3028fb5e99a
Oliver Freeman <oliver.freeman@bdi.ox.ac.uk>
2022-03-11 11:18:46 +0000

    Use the bootstap user rather than admin user

--- grails-app/init/uk/ac/ox/softeng/maurodatamapper/plugins/BootStrap.groovy
+++ grails-app/init/uk/ac/ox/softeng/maurodatamapper/plugins/BootStrap.groovy
@@ -18,51 +18,32 @@
 package uk.ac.ox.softeng.maurodatamapper.plugins
 
 import uk.ac.ox.softeng.maurodatamapper.core.admin.ApiProperty
-import uk.ac.ox.softeng.maurodatamapper.core.container.Folder
 import uk.ac.ox.softeng.maurodatamapper.plugins.nhsdd.NhsDataDictionaryService
+import uk.ac.ox.softeng.maurodatamapper.util.GormUtils
 
-import grails.core.GrailsApplication
+import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.context.MessageSource
 
-import static uk.ac.ox.softeng.maurodatamapper.core.bootstrap.StandardEmailAddress.ADMIN
-import static uk.ac.ox.softeng.maurodatamapper.core.bootstrap.StandardEmailAddress.getFUNCTIONAL_TEST
-
-/*
- * Copyright 2020-2022 University of Oxford and Health and Social Care Information Centre, also known as NHS Digital
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- *
- * SPDX-License-Identifier: Apache-2.0
- */
-
+import static uk.ac.ox.softeng.maurodatamapper.core.BootStrap.BootStrapUser
 
 class BootStrap {
+
+    @Autowired
+    MessageSource messageSource
 
     def init = {servletContext ->
         ApiProperty.withNewTransaction {
 
             List<ApiProperty> existingProperties = ApiProperty.findAllByCategory(NhsDataDictionaryService.NHSDD_PROPERTY_CATEGORY)
-            List<ApiProperty> newProperties = []
-
 
             NhsDataDictionaryService.KNOWN_KEYS.each {k, t ->
                 if (!existingProperties.find {it.key == k}) {
-                    newProperties.add(new ApiProperty(key: k, value: t,
-                                               createdBy: ADMIN,
-                                               category: NhsDataDictionaryService.NHSDD_PROPERTY_CATEGORY))
+                    ApiProperty apiProperty = new ApiProperty(key: k, value: t,
+                                                              createdBy: BootStrapUser.instance.emailAddress,
+                                                              category: NhsDataDictionaryService.NHSDD_PROPERTY_CATEGORY)
+                    GormUtils.checkAndSave(messageSource, apiProperty)
                 }
             }
-            ApiProperty.saveAll(newProperties)
         }
 
         environments {
