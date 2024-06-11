f30515cdbbe74f697fe406e6bf18939fb1a91e2e
OButlerOcc <oliver.butler@oxfordcc.co.uk>
2024-06-11 11:01:53 +0100

    gh-08 - pr feedback

--- grails-app/services/uk/ac/ox/softeng/maurodatamapper/plugins/nhsdd/profiles/DDExportControlProfileProviderService.groovy
+++ grails-app/services/uk/ac/ox/softeng/maurodatamapper/plugins/nhsdd/profiles/DDExportControlProfileProviderService.groovy
@@ -26,7 +26,7 @@ class DDExportControlProfileProviderService extends JsonProfileProviderService {
 
     @Override
     String getMetadataNamespace() {
-        'uk.nhs.datadictionary.data set'
+        'uk.nhs.datadictionary.dataSetExportControl'
     }
 
     @Override
