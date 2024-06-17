c423ff435e345eaa21d2653bf08fd251bda8204a
Peter Monks <Peter.Monks@oxfordcc.co.uk>
2024-06-17 16:58:33 +0100

    gh-14 Update PathChangeService to track some items in the dictionary that need updating
    - Scans for DataClasses, DataElements and Terms so far
    - Updates the description fields in these domain types to make sure a new source path is referenced in the string to not break links

--- grails-app/services/uk/ac/ox/softeng/maurodatamapper/plugins/nhsdd/PathChangeService.groovy
+++ grails-app/services/uk/ac/ox/softeng/maurodatamapper/plugins/nhsdd/PathChangeService.groovy
@@ -2,16 +2,29 @@ package uk.ac.ox.softeng.maurodatamapper.plugins.nhsdd
 
 import uk.ac.ox.softeng.maurodatamapper.core.async.AsyncJob
 import uk.ac.ox.softeng.maurodatamapper.core.async.AsyncJobService
+import uk.ac.ox.softeng.maurodatamapper.datamodel.item.DataClass
+import uk.ac.ox.softeng.maurodatamapper.datamodel.item.DataClassService
+import uk.ac.ox.softeng.maurodatamapper.datamodel.item.DataElement
+import uk.ac.ox.softeng.maurodatamapper.datamodel.item.DataElementService
 import uk.ac.ox.softeng.maurodatamapper.path.Path
 import uk.ac.ox.softeng.maurodatamapper.security.User
+import uk.ac.ox.softeng.maurodatamapper.terminology.item.Term
+import uk.ac.ox.softeng.maurodatamapper.terminology.item.TermService
 
 import grails.gorm.transactions.Transactional
 import groovy.util.logging.Slf4j
+import uk.nhs.digital.maurodatamapper.datadictionary.NhsDataDictionary
+import uk.nhs.digital.maurodatamapper.datadictionary.NhsDataDictionaryComponent
 
 @Slf4j
 @Transactional
 class PathChangeService {
     AsyncJobService asyncJobService
+    NhsDataDictionaryService nhsDataDictionaryService
+
+    DataClassService dataClassService
+    DataElementService dataElementService
+    TermService termService
 
     AsyncJob asyncModifyRelatedItemsAfterPathChange(UUID versionedFolderId, Path originalPath, Path updatedPath, User startedByUser) {
         asyncJobService.createAndSaveAsyncJob(
@@ -23,6 +36,128 @@ class PathChangeService {
     }
 
     void modifyRelatedItemsAfterPathChange(UUID versionedFolderId, Path originalPath, Path updatedPath) {
-        // TODO
+        // The NHS data dictionary ingest will have added paths without model identifiers (e.g. branches like "$main"),
+        // remove these so that we'll find the correct substrings to update
+        Path originalPathWithoutBranches = originalPath.clone()
+        originalPathWithoutBranches.pathNodes.forEach { it.modelIdentifier = "" }
+        String originalPathWithoutBranchesString = originalPathWithoutBranches.toString()
+
+        Path updatedPathWithoutBranches = updatedPath.clone()
+        updatedPathWithoutBranches.pathNodes.forEach { it.modelIdentifier = "" }
+        String updatedPathWithoutBranchesString = updatedPathWithoutBranches.toString()
+
+        log.info("Original path: $originalPathWithoutBranchesString")
+        log.info("Updated path: $updatedPathWithoutBranchesString")
+
+        log.debug("Building Data Dictionary...")
+        NhsDataDictionary dataDictionary = nhsDataDictionaryService.buildDataDictionary(versionedFolderId)
+
+        log.info("Scanning '$dataDictionary.containingVersionedFolder.label' [$dataDictionary.containingVersionedFolder.id]")
+        List<DataClass> dataClasses = []
+        List<DataElement> dataElements = []
+        List<Term> terms = []
+
+        scanNhsDataDictionary(
+            dataDictionary,
+            originalPathWithoutBranchesString,
+            dataClasses,
+            dataElements,
+            terms)
+
+        updateMauroDataClasses(dataClasses, originalPathWithoutBranchesString, updatedPathWithoutBranchesString)
+        updateMauroDataElements(dataElements, originalPathWithoutBranchesString, updatedPathWithoutBranchesString)
+        updateMauroTerms(terms, originalPathWithoutBranchesString, updatedPathWithoutBranchesString)
+    }
+
+    static void scanNhsDataDictionary(
+        NhsDataDictionary dataDictionary,
+        String originalPath,
+        List<DataClass> dataClasses,
+        List<DataElement> dataElements,
+        List<Term> terms) {
+
+        List<NhsDataDictionaryComponent> components = dataDictionary.allComponents
+
+        components.each { component ->
+            def catalogueItem = component.catalogueItem
+
+            if (catalogueItem instanceof DataClass) {
+                DataClass dataClass = (DataClass)catalogueItem
+                if (dataClass.description && dataClass.description.contains(originalPath)) {
+                    log.info("Requires update: DataClass '$dataClass.label' [$dataClass.id]")
+                    dataClasses.add(dataClass)
+                }
+            }
+
+            if (catalogueItem instanceof DataElement) {
+                DataElement dataElement = (DataElement)catalogueItem
+                if (dataElement.description && dataElement.description.contains(originalPath)) {
+                    log.info("Requires update: DataElement '$dataElement.label' [$dataElement.id]")
+                    dataElements.add(dataElement)
+                }
+            }
+
+            if (catalogueItem instanceof Term) {
+                Term term = (Term)catalogueItem
+                if (term.description && term.description.contains(originalPath)) {
+                    log.info("Requires update: Term '$term.label' [$term.id]")
+                    terms.add(term)
+                }
+            }
+        }
+    }
+
+    void updateMauroDataClasses(List<DataClass> dataClasses, String originalPath, String updatedPath) {
+        if (dataClasses.empty) {
+            return
+        }
+
+        dataClasses.each { dataClass ->
+            dataClass.description = dataClass.description.replace(originalPath, updatedPath)
+
+            try {
+                dataClassService.save([flush: true, validate: true], dataClass)
+                log.info("Updated DataClass '$dataClass.label' [$dataClass.id]")
+            }
+            catch (Exception exception) {
+                log.error("Exception occurred updating DataClass '$dataClass.label' [$dataClass.id]: $exception.message")
+            }
+        }
+    }
+
+    void updateMauroDataElements(List<DataElement> dataElements, String originalPath, String updatedPath) {
+        if (dataElements.empty) {
+            return
+        }
+
+        dataElements.each { dataElement ->
+            dataElement.description = dataElement.description.replace(originalPath, updatedPath)
+
+            try {
+                dataElementService.save([flush: true, validate: true], dataElement)
+                log.info("Updated DataElement '$dataElement.label' [$dataElement.id]")
+            }
+            catch (Exception exception) {
+                log.error("Exception occurred updating DataElement '$dataElement.label' [$dataElement.id]: $exception.message")
+            }
+        }
+    }
+
+    void updateMauroTerms(List<Term> terms, String originalPath, String updatedPath) {
+        if (terms.empty) {
+            return
+        }
+
+        terms.each { term ->
+            term.description = term.description.replace(originalPath, updatedPath)
+
+            try {
+                termService.save([flush: true, validate: true], term)
+                log.info("Updated Term '$term.label' [$term.id]")
+            }
+            catch (Exception exception) {
+                log.error("Exception occurred updating Term '$term.label' [$term.id]: $exception.message")
+            }
+        }
     }
 }
