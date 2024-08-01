057be21b9ad9aaa279b8e647020d3bbfa0d4cccb
Peter Monks <Peter.Monks@oxfordcc.co.uk>
2024-08-01 09:40:26 +0100

    gh-75 WIP Add test to check updated description changes appear in change papers

--- src/integration-test/groovy/nhs/digital/maurodatamapper/datadictionary/ChangePaperChangesSpec.groovy
+++ src/integration-test/groovy/nhs/digital/maurodatamapper/datadictionary/ChangePaperChangesSpec.groovy
@@ -47,4 +47,53 @@ class NhsDDAttributeChangePaperChangesSpec extends ChangePaperChangesSpec  {
             "</div>"
         }
     }
+
+    void "should return changes for an updated description"() {
+        given: "there is a previous component"
+        NhsDDAttribute previousComponent = new NhsDDAttribute(
+            name: "DIAGNOSTIC TEST REQUEST RECEIVED TIME",
+            definition: "The time the <a href=\"DIAGNOSTIC TEST REQUEST\">DIAGNOSTIC TEST REQUEST</a> was received.")
+
+        and: "there is a current component"
+        NhsDDAttribute currentComponent = new NhsDDAttribute(
+            name: "DIAGNOSTIC TEST REQUEST RECEIVED TIME",
+            definition: "The time the <a href=\"DIAGNOSTIC TEST REQUEST\">DIAGNOSTIC TEST REQUEST</a> was obtained.")
+
+        when: "finding the changes"
+        List<Change> changes = currentComponent.getChanges(previousComponent)
+
+        then: "the expected change object is returned"
+        Change change = changes.first()
+        with {
+            change
+        }
+
+        String ditaXml = change.ditaDetail.toXmlString()
+
+        verifyAll(change) {
+            changeType == UPDATED_DESCRIPTION_TYPE
+            stereotype == "Attribute"
+            oldItem == previousComponent
+            newItem == currentComponent
+            htmlDetail == "<div>\n" +
+            "  <div>The time the <a href=\"DIAGNOSTIC TEST REQUEST\">DIAGNOSTIC TEST REQUEST</a> was <span class=\"diff-html-removed\" id=\"removed-diff-0\" previous=\"first-diff\" changeId=\"removed-diff-0\" next=\"added-diff-0\">received</span><span class=\"diff-html-added\" id=\"added-diff-0\" previous=\"removed-diff-0\" changeId=\"added-diff-0\" next=\"last-diff\">obtained</span>.</div>\n" +
+            "</div>"
+            ditaXml == """<div>
+  <div outputclass='deleted'>
+    <div>The time the 
+
+      <xref keyref='DIAGNOSTIC%20TEST%20REQUEST' scope='local'>DIAGNOSTIC TEST REQUEST</xref> was received.
+
+    </div>
+  </div>
+  <div outputclass='new'>
+    <div>The time the 
+
+      <xref keyref='DIAGNOSTIC%20TEST%20REQUEST' scope='local'>DIAGNOSTIC TEST REQUEST</xref> was obtained.
+
+    </div>
+  </div>
+</div>"""
+        }
+    }
 }
