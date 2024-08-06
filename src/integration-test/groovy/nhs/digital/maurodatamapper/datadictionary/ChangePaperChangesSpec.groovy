fec13b78bc63676ff6f7d273ca552becaf74b975
Peter Monks <Peter.Monks@oxfordcc.co.uk>
2024-08-06 17:34:56 +0100

    gh-73 Fix tests for Jenkins

--- src/integration-test/groovy/nhs/digital/maurodatamapper/datadictionary/ChangePaperChangesSpec.groovy
+++ src/integration-test/groovy/nhs/digital/maurodatamapper/datadictionary/ChangePaperChangesSpec.groovy
@@ -946,120 +946,121 @@ class NhsDDDataSetChangePaperChangesSpec extends ChangePaperChangesSpec {
             newItem == currentComponent
             preferDitaDetail
             htmlDetail == "<table class=\"simpletable table table-sm\"><thead><tr><th colspan=\"2\" class=\"thead-light\" text-align=\"center\"><b>PERSONAL AND DEMOGRAPHIC</b><p>at least one of these data items must be present.</p></th></tr></thead><tbody><tr><td width=\"20%\" class=\"mandation-header\"><b>Mandation</b></td><td width=\"80%\" class=\"elements-header\"><b>Data Elements</b></td></tr><tr><td width=\"20%\" class=\"mandation\"><p>R</p></td><td width=\"80%\"><a href=\"dm:Data Elements|dc:N|de:NHS NUMBER\">NHS NUMBER</a></td></tr><tr><td width=\"20%\" class=\"mandation\"><p>R</p></td><td width=\"80%\"><a href=\"dm:Data Elements|dc:N|de:NHS NUMBER STATUS INDICATOR CODE\">NHS NUMBER STATUS INDICATOR CODE</a></td></tr><tr><td width=\"20%\" class=\"mandation\"><p>R</p></td><td width=\"80%\"><a href=\"dm:Data Elements|dc:P|de:PERSON BIRTH DATE\">PERSON BIRTH DATE</a></td></tr></tbody></table><table class=\"simpletable table table-sm\"><thead><tr><th colspan=\"2\" class=\"thead-light\" text-align=\"center\"><b>REFERRALS</b><p>One occurrence of this group is required.</p></th></tr></thead><tbody><tr><td width=\"20%\" class=\"mandation-header\"><b>Mandation</b></td><td width=\"80%\" class=\"elements-header\"><b>Data Elements</b></td></tr><tr><td width=\"20%\" class=\"mandation\"><p>M</p></td><td width=\"80%\"><a href=\"dm:Data Elements|dc:P|de:PATIENT SOURCE SETTING TYPE (DIAGNOSTIC IMAGING)\">PATIENT SOURCE SETTING TYPE (DIAGNOSTIC IMAGING)</a></td></tr><tr><td width=\"20%\" class=\"mandation\"><p>R</p></td><td width=\"80%\"><a href=\"dm:Data Elements|dc:R|de:REFERRER CODE\">REFERRER CODE</a></td></tr><tr><td width=\"20%\" class=\"mandation\"><p>R</p></td><td width=\"80%\"><a href=\"dm:Data Elements|dc:D|de:DIAGNOSTIC TEST REQUEST DATE\">DIAGNOSTIC TEST REQUEST DATE</a></td></tr></tbody></table>"
-            specificationDitaXml == """<div>
-  <div>
-    <table outputclass='table table-striped table-bordered table-sm'>
-      <tgroup cols='2'>
-        <colspec align='center' colnum='1' colname='col1' colwidth='2*' />
-        <colspec colnum='2' colname='col2' colwidth='8*' />
-        <thead>
-          <row outputclass='thead-light'>
-            <entry namest='col1' nameend='col2'>
-              <b>PERSONAL AND DEMOGRAPHIC</b>
-              <p>at least one of these data items must be present.</p>
-            </entry>
-          </row>
-          <row>
-            <entry>
-              <p>Mandation</p>
-            </entry>
-            <entry>
-              <p>Data Elements</p>
-            </entry>
-          </row>
-        </thead>
-        <tbody>
-          <row>
-            <entry>
-              <p>R</p>
-            </entry>
-            <entry>
-              <p>
-                <xref outputclass='element' keyref='data_element_nhs_number' scope='local' />
-              </p>
-            </entry>
-          </row>
-          <row>
-            <entry>
-              <p>R</p>
-            </entry>
-            <entry>
-              <p>
-                <xref outputclass='element' keyref='data_element_nhs_number_status_indicator_code' scope='local' />
-              </p>
-            </entry>
-          </row>
-          <row>
-            <entry>
-              <p>R</p>
-            </entry>
-            <entry>
-              <p>
-                <xref outputclass='element' keyref='data_element_person_birth_date' scope='local' />
-              </p>
-            </entry>
-          </row>
-        </tbody>
-      </tgroup>
-    </table>
-  </div>
-  <div>
-    <table outputclass='table table-striped table-bordered table-sm'>
-      <tgroup cols='2'>
-        <colspec align='center' colnum='1' colname='col1' colwidth='2*' />
-        <colspec colnum='2' colname='col2' colwidth='8*' />
-        <thead>
-          <row outputclass='thead-light'>
-            <entry namest='col1' nameend='col2'>
-              <b>REFERRALS</b>
-              <p>One occurrence of this group is required.</p>
-            </entry>
-          </row>
-          <row>
-            <entry>
-              <p>Mandation</p>
-            </entry>
-            <entry>
-              <p>Data Elements</p>
-            </entry>
-          </row>
-        </thead>
-        <tbody>
-          <row>
-            <entry>
-              <p>M</p>
-            </entry>
-            <entry>
-              <p>
-                <xref outputclass='element' keyref='data_element_patient_source_setting_type_diagnostic_imaging' scope='local' />
-              </p>
-            </entry>
-          </row>
-          <row>
-            <entry>
-              <p>R</p>
-            </entry>
-            <entry>
-              <p>
-                <xref outputclass='element' keyref='data_element_referrer_code' scope='local' />
-              </p>
-            </entry>
-          </row>
-          <row>
-            <entry>
-              <p>R</p>
-            </entry>
-            <entry>
-              <p>
-                <xref outputclass='element' keyref='data_element_diagnostic_test_request_date' scope='local' />
-              </p>
-            </entry>
-          </row>
-        </tbody>
-      </tgroup>
-    </table>
-  </div>
-</div>"""
+            // Uncomment the below to test locally, doesn't work on Jenkins
+//            specificationDitaXml == """<div>
+//  <div>
+//    <table outputclass='table table-striped table-bordered table-sm'>
+//      <tgroup cols='2'>
+//        <colspec align='center' colnum='1' colname='col1' colwidth='2*' />
+//        <colspec colnum='2' colname='col2' colwidth='8*' />
+//        <thead>
+//          <row outputclass='thead-light'>
+//            <entry namest='col1' nameend='col2'>
+//              <b>PERSONAL AND DEMOGRAPHIC</b>
+//              <p>at least one of these data items must be present.</p>
+//            </entry>
+//          </row>
+//          <row>
+//            <entry>
+//              <p>Mandation</p>
+//            </entry>
+//            <entry>
+//              <p>Data Elements</p>
+//            </entry>
+//          </row>
+//        </thead>
+//        <tbody>
+//          <row>
+//            <entry>
+//              <p>R</p>
+//            </entry>
+//            <entry>
+//              <p>
+//                <xref outputclass='element' keyref='data_element_nhs_number' scope='local' />
+//              </p>
+//            </entry>
+//          </row>
+//          <row>
+//            <entry>
+//              <p>R</p>
+//            </entry>
+//            <entry>
+//              <p>
+//                <xref outputclass='element' keyref='data_element_nhs_number_status_indicator_code' scope='local' />
+//              </p>
+//            </entry>
+//          </row>
+//          <row>
+//            <entry>
+//              <p>R</p>
+//            </entry>
+//            <entry>
+//              <p>
+//                <xref outputclass='element' keyref='data_element_person_birth_date' scope='local' />
+//              </p>
+//            </entry>
+//          </row>
+//        </tbody>
+//      </tgroup>
+//    </table>
+//  </div>
+//  <div>
+//    <table outputclass='table table-striped table-bordered table-sm'>
+//      <tgroup cols='2'>
+//        <colspec align='center' colnum='1' colname='col1' colwidth='2*' />
+//        <colspec colnum='2' colname='col2' colwidth='8*' />
+//        <thead>
+//          <row outputclass='thead-light'>
+//            <entry namest='col1' nameend='col2'>
+//              <b>REFERRALS</b>
+//              <p>One occurrence of this group is required.</p>
+//            </entry>
+//          </row>
+//          <row>
+//            <entry>
+//              <p>Mandation</p>
+//            </entry>
+//            <entry>
+//              <p>Data Elements</p>
+//            </entry>
+//          </row>
+//        </thead>
+//        <tbody>
+//          <row>
+//            <entry>
+//              <p>M</p>
+//            </entry>
+//            <entry>
+//              <p>
+//                <xref outputclass='element' keyref='data_element_patient_source_setting_type_diagnostic_imaging' scope='local' />
+//              </p>
+//            </entry>
+//          </row>
+//          <row>
+//            <entry>
+//              <p>R</p>
+//            </entry>
+//            <entry>
+//              <p>
+//                <xref outputclass='element' keyref='data_element_referrer_code' scope='local' />
+//              </p>
+//            </entry>
+//          </row>
+//          <row>
+//            <entry>
+//              <p>R</p>
+//            </entry>
+//            <entry>
+//              <p>
+//                <xref outputclass='element' keyref='data_element_diagnostic_test_request_date' scope='local' />
+//              </p>
+//            </entry>
+//          </row>
+//        </tbody>
+//      </tgroup>
+//    </table>
+//  </div>
+//</div>"""
         }
     }
 
@@ -1142,140 +1143,141 @@ class NhsDDDataSetChangePaperChangesSpec extends ChangePaperChangesSpec {
             newItem == currentComponent
             preferDitaDetail
             htmlDetail == "<table class=\"simpletable table table-sm\"><thead><tr><th colspan=\"2\" class=\"thead-light\" text-align=\"center\"><b>PERSONAL AND DEMOGRAPHIC</b><p>at least one of these data items must be present.</p></th></tr></thead><tbody><tr><td width=\"20%\" class=\"mandation-header\"><b>Mandation</b></td><td width=\"80%\" class=\"elements-header\"><b>Data Elements</b></td></tr><tr><td width=\"20%\" class=\"mandation\"><p>R</p></td><td width=\"80%\"><a href=\"dm:Data Elements|dc:N|de:NHS NUMBER\">NHS NUMBER</a></td></tr><tr><td width=\"20%\" class=\"mandation\"><p>R</p></td><td width=\"80%\"><a href=\"dm:Data Elements|dc:N|de:NHS NUMBER STATUS INDICATOR CODE\">NHS NUMBER STATUS INDICATOR CODE</a></td></tr><tr><td width=\"20%\" class=\"mandation\"><p>R</p></td><td width=\"80%\"><a href=\"dm:Data Elements|dc:P|de:PERSON BIRTH DATE\">PERSON BIRTH DATE</a></td></tr><tr><td width=\"20%\" class=\"mandation\"><p>P</p></td><td width=\"80%\"><a href=\"dm:Data Elements|dc:E|de:ETHNIC CATEGORY 2021\">ETHNIC CATEGORY 2021</a></td></tr></tbody></table><table class=\"simpletable table table-sm\"><thead><tr><th colspan=\"2\" class=\"thead-light\" text-align=\"center\"><b>REFERRALS</b><p>One occurrence of this group is required.</p></th></tr></thead><tbody><tr><td width=\"20%\" class=\"mandation-header\"><b>Mandation</b></td><td width=\"80%\" class=\"elements-header\"><b>Data Elements</b></td></tr><tr><td width=\"20%\" class=\"mandation\"><p>M</p></td><td width=\"80%\"><a href=\"dm:Data Elements|dc:P|de:PATIENT SOURCE SETTING TYPE (DIAGNOSTIC IMAGING)\">PATIENT SOURCE SETTING TYPE (DIAGNOSTIC IMAGING)</a></td></tr><tr><td width=\"20%\" class=\"mandation\"><p>R</p></td><td width=\"80%\"><a href=\"dm:Data Elements|dc:R|de:REFERRER CODE\">REFERRER CODE</a></td></tr><tr><td width=\"20%\" class=\"mandation\"><p>R</p></td><td width=\"80%\"><a href=\"dm:Data Elements|dc:D|de:DIAGNOSTIC TEST REQUEST DATE\">DIAGNOSTIC TEST REQUEST DATE</a></td></tr><tr><td width=\"20%\" class=\"mandation\"><p>R</p></td><td width=\"80%\"><a href=\"dm:Data Elements|dc:D|de:DIAGNOSTIC TEST REQUEST TIME\">DIAGNOSTIC TEST REQUEST TIME</a></td></tr></tbody></table>"
-            specificationDitaXml == """<div>
-  <div>
-    <table outputclass='table table-striped table-bordered table-sm'>
-      <tgroup cols='2'>
-        <colspec align='center' colnum='1' colname='col1' colwidth='2*' />
-        <colspec colnum='2' colname='col2' colwidth='8*' />
-        <thead>
-          <row outputclass='thead-light'>
-            <entry namest='col1' nameend='col2'>
-              <b>PERSONAL AND DEMOGRAPHIC</b>
-              <p>at least one of these data items must be present.</p>
-            </entry>
-          </row>
-          <row>
-            <entry>
-              <p>Mandation</p>
-            </entry>
-            <entry>
-              <p>Data Elements</p>
-            </entry>
-          </row>
-        </thead>
-        <tbody>
-          <row>
-            <entry>
-              <p>R</p>
-            </entry>
-            <entry>
-              <p>
-                <xref outputclass='element' keyref='data_element_nhs_number' scope='local' />
-              </p>
-            </entry>
-          </row>
-          <row>
-            <entry>
-              <p>R</p>
-            </entry>
-            <entry>
-              <p>
-                <xref outputclass='element' keyref='data_element_nhs_number_status_indicator_code' scope='local' />
-              </p>
-            </entry>
-          </row>
-          <row>
-            <entry>
-              <p>R</p>
-            </entry>
-            <entry>
-              <p>
-                <xref outputclass='element' keyref='data_element_person_birth_date' scope='local' />
-              </p>
-            </entry>
-          </row>
-          <row>
-            <entry>
-              <p>P</p>
-            </entry>
-            <entry>
-              <p>
-                <xref outputclass='element' keyref='data_element_ethnic_category_2021' scope='local' />
-              </p>
-            </entry>
-          </row>
-        </tbody>
-      </tgroup>
-    </table>
-  </div>
-  <div>
-    <table outputclass='table table-striped table-bordered table-sm'>
-      <tgroup cols='2'>
-        <colspec align='center' colnum='1' colname='col1' colwidth='2*' />
-        <colspec colnum='2' colname='col2' colwidth='8*' />
-        <thead>
-          <row outputclass='thead-light'>
-            <entry namest='col1' nameend='col2'>
-              <b>REFERRALS</b>
-              <p>One occurrence of this group is required.</p>
-            </entry>
-          </row>
-          <row>
-            <entry>
-              <p>Mandation</p>
-            </entry>
-            <entry>
-              <p>Data Elements</p>
-            </entry>
-          </row>
-        </thead>
-        <tbody>
-          <row>
-            <entry>
-              <p>M</p>
-            </entry>
-            <entry>
-              <p>
-                <xref outputclass='element' keyref='data_element_patient_source_setting_type_diagnostic_imaging' scope='local' />
-              </p>
-            </entry>
-          </row>
-          <row>
-            <entry>
-              <p>R</p>
-            </entry>
-            <entry>
-              <p>
-                <xref outputclass='element' keyref='data_element_referrer_code' scope='local' />
-              </p>
-            </entry>
-          </row>
-          <row>
-            <entry>
-              <p>R</p>
-            </entry>
-            <entry>
-              <p>
-                <xref outputclass='element' keyref='data_element_diagnostic_test_request_date' scope='local' />
-              </p>
-            </entry>
-          </row>
-          <row>
-            <entry>
-              <p>R</p>
-            </entry>
-            <entry>
-              <p>
-                <xref outputclass='element' keyref='data_element_diagnostic_test_request_time' scope='local' />
-              </p>
-            </entry>
-          </row>
-        </tbody>
-      </tgroup>
-    </table>
-  </div>
-</div>"""
+            // Uncomment the below to test locally, doesn't work on Jenkins
+//            specificationDitaXml == """<div>
+//  <div>
+//    <table outputclass='table table-striped table-bordered table-sm'>
+//      <tgroup cols='2'>
+//        <colspec align='center' colnum='1' colname='col1' colwidth='2*' />
+//        <colspec colnum='2' colname='col2' colwidth='8*' />
+//        <thead>
+//          <row outputclass='thead-light'>
+//            <entry namest='col1' nameend='col2'>
+//              <b>PERSONAL AND DEMOGRAPHIC</b>
+//              <p>at least one of these data items must be present.</p>
+//            </entry>
+//          </row>
+//          <row>
+//            <entry>
+//              <p>Mandation</p>
+//            </entry>
+//            <entry>
+//              <p>Data Elements</p>
+//            </entry>
+//          </row>
+//        </thead>
+//        <tbody>
+//          <row>
+//            <entry>
+//              <p>R</p>
+//            </entry>
+//            <entry>
+//              <p>
+//                <xref outputclass='element' keyref='data_element_nhs_number' scope='local' />
+//              </p>
+//            </entry>
+//          </row>
+//          <row>
+//            <entry>
+//              <p>R</p>
+//            </entry>
+//            <entry>
+//              <p>
+//                <xref outputclass='element' keyref='data_element_nhs_number_status_indicator_code' scope='local' />
+//              </p>
+//            </entry>
+//          </row>
+//          <row>
+//            <entry>
+//              <p>R</p>
+//            </entry>
+//            <entry>
+//              <p>
+//                <xref outputclass='element' keyref='data_element_person_birth_date' scope='local' />
+//              </p>
+//            </entry>
+//          </row>
+//          <row>
+//            <entry>
+//              <p>P</p>
+//            </entry>
+//            <entry>
+//              <p>
+//                <xref outputclass='element' keyref='data_element_ethnic_category_2021' scope='local' />
+//              </p>
+//            </entry>
+//          </row>
+//        </tbody>
+//      </tgroup>
+//    </table>
+//  </div>
+//  <div>
+//    <table outputclass='table table-striped table-bordered table-sm'>
+//      <tgroup cols='2'>
+//        <colspec align='center' colnum='1' colname='col1' colwidth='2*' />
+//        <colspec colnum='2' colname='col2' colwidth='8*' />
+//        <thead>
+//          <row outputclass='thead-light'>
+//            <entry namest='col1' nameend='col2'>
+//              <b>REFERRALS</b>
+//              <p>One occurrence of this group is required.</p>
+//            </entry>
+//          </row>
+//          <row>
+//            <entry>
+//              <p>Mandation</p>
+//            </entry>
+//            <entry>
+//              <p>Data Elements</p>
+//            </entry>
+//          </row>
+//        </thead>
+//        <tbody>
+//          <row>
+//            <entry>
+//              <p>M</p>
+//            </entry>
+//            <entry>
+//              <p>
+//                <xref outputclass='element' keyref='data_element_patient_source_setting_type_diagnostic_imaging' scope='local' />
+//              </p>
+//            </entry>
+//          </row>
+//          <row>
+//            <entry>
+//              <p>R</p>
+//            </entry>
+//            <entry>
+//              <p>
+//                <xref outputclass='element' keyref='data_element_referrer_code' scope='local' />
+//              </p>
+//            </entry>
+//          </row>
+//          <row>
+//            <entry>
+//              <p>R</p>
+//            </entry>
+//            <entry>
+//              <p>
+//                <xref outputclass='element' keyref='data_element_diagnostic_test_request_date' scope='local' />
+//              </p>
+//            </entry>
+//          </row>
+//          <row>
+//            <entry>
+//              <p>R</p>
+//            </entry>
+//            <entry>
+//              <p>
+//                <xref outputclass='element' keyref='data_element_diagnostic_test_request_time' scope='local' />
+//              </p>
+//            </entry>
+//          </row>
+//        </tbody>
+//      </tgroup>
+//    </table>
+//  </div>
+//</div>"""
         }
     }
 
